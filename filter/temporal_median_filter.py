6b4002c92a90f623c08c81ff8762a4a28675e055
Charlie O'Hara <charlie.b.ohara@gmail.com>
2018-09-18 20:59:17 -0700

    refactor temporal median filter

--- filter/temporal_median_filter.py
+++ filter/temporal_median_filter.py
@@ -4,18 +4,18 @@ import numpy as np
 class TemporalMedianFilter:
 	def __init__(self, number_of_previous_scans):
 		self.D = number_of_previous_scans
-		self.all_input_scans = None
+		self.input_scans = None
 
 	def update(self, input_scan):
-		if self.all_input_scans is None:
-			self.all_input_scans = input_scan
-			return self.all_input_scans
+		if self.input_scans is None:
+			self.input_scans = input_scan
+			return self.input_scans
 
-		self.all_input_scans = np.vstack((self.all_input_scans, input_scan))
+		self.input_scans = np.vstack((self.input_scans, input_scan))
 
-		num_matrix_rows = np.shape(self.all_input_scans)[0]
+		num_matrix_rows = np.shape(self.input_scans)[0]
 		num_rows_to_include = self.D + 1
 		if num_matrix_rows > num_rows_to_include:
-			self.all_input_scans = np.delete(self.all_input_scans, 0, 0)
+			self.input_scans = np.delete(self.input_scans, 0, 0)
 
-		return np.median(self.all_input_scans, axis=0)
+		return np.median(self.input_scans, axis=0)
