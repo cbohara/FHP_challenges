ca4fa2e1b47c5c193f0c68c685232b5980d96ea7
Charlie O'Hara <charlie.b.ohara@gmail.com>
2018-09-19 17:54:57 -0700

    add docstrings

--- filter/range_filter.py
+++ filter/range_filter.py
@@ -3,10 +3,36 @@ import numpy as np
 
 class RangeFilter:
 	def __init__(self, range_min_value, range_max_value):
+		"""
+		Initialize RangeFilter object.
+
+		Parameters
+		----------
+		range_min_value: int
+			Minimum value to include in filtered array.
+		range_max_value: int
+			Maximum value to include in filtered array.
+		"""
 		self.range_min_value = range_min_value
 		self.range_max_value = range_max_value
 
 	def update(self, input_scan):
+		"""
+		Replace values out of range with boundary values.
+
+		Replace values below range_min_value with range_min_value.
+		Replace values above range_max_value with range_max_value.
+
+		Parameters
+		----------
+		input_scan: numpy array
+			Array of float values representing distance measurements.
+
+		Returns
+		-------
+		numpy array
+			Filtered array containing values within desired range.
+		"""
 		filtered_scan = []
 		for value in input_scan:
 			if value < self.range_min_value:
--- filter/temporal_median_filter.py
+++ filter/temporal_median_filter.py
@@ -3,10 +3,37 @@ import numpy as np
 
 class TemporalMedianFilter:
 	def __init__(self, number_of_previous_scans):
+		"""
+		Initialize TemporalMedianFilter object.
+
+		Parameters
+		----------
+		number_of_previous_scans: int
+			Number of previous input scans to include for median calculation.
+		"""
 		self.D = number_of_previous_scans
 		self.input_scans = None
 
 	def update(self, input_scan):
+		"""
+		Determine the median of the current and the previous scans.
+
+		The median is calculated for each element in the array.
+		The TemporalMedianFilter object previous D scans parameter
+		determines how many previous input scans to include when
+		calculating the median of each element.
+
+		Parameters
+		----------
+		input_scan: numpy array
+			Single array of float values representing distance measurements.
+
+		Returns
+		-------
+		numpy array
+			Filtered array containing median value of each element.
+		"""
+
 		if self.input_scans is None:
 			self.input_scans = input_scan
 			return self.input_scans
